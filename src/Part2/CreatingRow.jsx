db9fbba4db439b76f76ca4414534fdc630a34dfc
Manoj Kumar Rai <iammanojkumarrai@gmail.com>
2026-01-16 09:37:40 +0530

    Checking Warning

--- src/Part2/CreatingRow.jsx
+++ src/Part2/CreatingRow.jsx
@@ -7,11 +7,17 @@ export default function CreateingRow() {
 
 const tablesDB = new TablesDB(client);
 
+const data = {
+    name: "Rahul Rai",
+        email: "iamrahulrai123@gmail.com",
+        password: "poptato@1234"
+}
+
 const promise = tablesDB.createRow({
     databaseId: import.meta.env.VITE_APPWRITE_DATABASE_ID,
-    tableId: import.meta.env.VITE_APPWRITE_COLLECTION_ID_NOTES,
+    tableId: '6967837f0036d2876346',
     rowId: ID.unique(),
-    data: { title: "Hamlet" }
+    data: data
 });
 
 promise.then(function (response) {
